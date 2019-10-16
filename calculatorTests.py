93e3a6ebb5bad526929162cbe6f4fd2e8a59c8ee
Nikshan Patel <nikshanpatel@gmail.com>
2019-10-16 19:07:26 -0400

    Task: Updated the unit test class

--- calculatorTests.py
+++ calculatorTests.py
@@ -5,8 +5,12 @@ from calculator import Calculator
 class MyTestCase(unittest.TestCase):
 
     def test_instantiate_calc(self):
-        calc = Calculator
+        calc = Calculator()
         self.assertIsInstance(calc, Calculator)
+
+    def test_result_property_calc(self):
+        calc = Calculator()
+        self.assertEqual(calc.result, 4)
 
 
 if __name__ == '__main__':
