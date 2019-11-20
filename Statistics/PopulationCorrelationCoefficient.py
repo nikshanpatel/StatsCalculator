8ee8c5145280e36878d7c688486aa617e889a69c
Nikshan Patel <nikshanpatel@gmail.com>
2019-11-19 22:25:56 -0500

    TASK - Updated Statistics Unit Testing

--- Statistics/PopulationCorrelationCoefficient.py
+++ Statistics/PopulationCorrelationCoefficient.py
@@ -8,10 +8,10 @@ from Calculator.Addition import addition
 
 def population_correlation_coefficient(list_x, list_y):
     total = 0
-    σ_x = standard_deviation(list_x)
-    σ_y = standard_deviation(list_y)
-    for i in list_x:
+    x = standard_deviation(list_x)
+    y = standard_deviation(list_y)
+    for i in range(len(list_x)):
         diff_x = subtraction(list_x[i], mean(list_x))
         diff_y = subtraction(list_y[i], mean(list_y))
-        total = total + multiplication(division(diff_x, σ_x), division(diff_y, σ_y))
-    return multiplication(division(1, addition(len(list_x), len(list_y))), total)
+        total = total + multiplication(division(diff_x, x), division(diff_y, y))
+    return round(float(multiplication(division(1, addition(len(list_x), len(list_y))), total)),4)
