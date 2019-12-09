667e909bf6b110789ba589d2766f46f0291df4ce
Nikshan Patel <nikshanpatel@gmail.com>
2019-12-08 22:37:16 -0500

    FEATURE - Create Tables Customers, Items, Orders, Order_lines

--- Database/sqlite_create_tables.py
+++ Database/sqlite_create_tables.py
@@ -1,44 +1,52 @@
-from sqlalchemy import create_engine, MetaData, Table, Integer, String
-from sqlalchemy import Column, DateTime, ForeignKey, Numeric, CheckConstraint
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
+    Column, DateTime, ForeignKey, Numeric, SmallInteger
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from datetime import datetime
-
-metadata = MetaData()
 
 engine = create_engine('sqlite:////web/Sqlite-Data/example.db')
 
-customers = Table('customers', metadata,
-                  Column('id', Integer(), primary_key=True),
-                  Column('first_name', String(100), nullable=False),
-                  Column('last_name', String(100), nullable=False),
-                  Column('username', String(50), nullable=False),
-                  Column('email', String(200), nullable=False),
-                  Column('address', String(200), nullable=False),
-                  Column('town', String(50), nullable=False),
-                  Column('created_on', DateTime(), default=datetime.now),
-                  Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now)
-                  )
+Base = declarative_base()
 
-items = Table('items', metadata,
-              Column('id', Integer(), primary_key=True),
-              Column('name', String(200), nullable=False),
-              Column('cost_price', Numeric(10, 2), nullable=False),
-              Column('selling_price', Numeric(10, 2), nullable=False),
-              Column('quantity', Integer(), nullable=False),
-              CheckConstraint('quantity > 0', name='quantity_check')
-              )
 
-orders = Table('orders', metadata,
-               Column('id', Integer(), primary_key=True),
-               Column('customer_id', ForeignKey('customers.id')),
-               Column('date_placed', DateTime(), default=datetime.now),
-               Column('date_shipped', DateTime())
-               )
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    username = Column(String(50), nullable=False)
+    email = Column(String(200), nullable=False)
+    created_on = Column(DateTime(), default=datetime.now)
+    updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+    orders = relationship("Order", backref='customer')
 
-order_lines = Table('order_lines', metadata,
-                    Column('id', Integer(), primary_key=True),
-                    Column('order_id', ForeignKey('orders.id')),
-                    Column('item_id', ForeignKey('items.id')),
-                    Column('quantity', Integer())
-                    )
 
-metadata.create_all(engine)
+class Item(Base):
+    __tablename__ = 'items'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(200), nullable=False)
+    cost_price = Column(Numeric(10, 2), nullable=False)
+    selling_price = Column(Numeric(10, 2), nullable=False)
+
+
+#     orders = relationship("Order", backref='customer')
+
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer(), primary_key=True)
+    customer_id = Column(Integer(), ForeignKey('customers.id'))
+    date_placed = Column(DateTime(), default=datetime.now)
+    line_items = relationship("OrderLine", secondary="order_lines", backref='order')
+
+
+class OrderLine(Base):
+    __tablename__ = 'order_lines'
+    id = Column(Integer(), primary_key=True)
+    order_id = Column(Integer(), ForeignKey('orders.id'))
+    item_id = Column(Integer(), ForeignKey('items.id'))
+    quantity = Column(SmallInteger())
+    item = relationship("Item")
+
+
+Base.metadata.create_all(engine)
